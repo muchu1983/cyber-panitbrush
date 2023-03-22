8101dd750eeeb15e8234eb73c79fdd51d31445cb
muchu1983 <muchu1983@gmail.com>
2023-03-22 17:35:19 +0800

    change emoji to real word

--- src/core/cyber_paintbrush.py
+++ src/core/cyber_paintbrush.py
@@ -3,25 +3,35 @@
 """" """
 import os
 
-strEmojiList = ["((😀))","((🤬))","((😂))","((🙄))","((😴))","((😍))","((😜))","((😆))",\
-	"((😉))","((😵))","((😠))","((😎))","((🥱))","((🥶))","((🤮))","((😶))",\
-	"((😐))","((😨))","((🤡))","((😭))","((👿))","((💀))","((😇))","((😏))"]
+#strEmojiList = ["((😀))","((🤬))","((😂))","((🙄))","((😴))","((😍))","((😜))","((😆))",\
+#	"((😉))","((😵))","((😠))","((😎))","((🥱))","((🥶))","((🤮))","((😶))",\
+#	"((😐))","((😨))","((🤡))","((😭))","((👿))","((💀))","((😇))","((😏))"]
+strEmojiList = ["blush", "tears", "tongue out", "grin", "aqua eyes", "fangs", "heart-shaped pupils",\
+ "angry", "jitome", "pout", "excited", "crazy", "sigh", "heart in eye", "shy", "seductive smile",\
+ "sad", "sleepy", "nosebleed", "drunk", "dark_persona", "embarrassed", "cute face"]
 
-strNegative = "ng_deepnegative_v1_75t,  easynegative, (worst quality, low quality:1.4), logo, text, monochrome, NSFW, mutated hands and fingers, poorly drawn hands, ((bad anatomy))"
+strNegative = "ng_deepnegative_v1_75t,  easynegative,\
+	(worst quality, low quality:1.4),\
+	logo, text, monochrome, NSFW,\
+	mutated hands and fingers, poorly drawn hands, ((bad anatomy)),\
+	cockeye, walleye"
 
-strBeforeEmoji = "masterpiece, best quality, highres, line art, comic, anime, colorful, \
-	 solo, upper body, kawaii, 1girl, small breasts, Age:18, big head style, detailed eyes, \
-	 close front shoot symmetrical photo portrait"
-strAfterEmoji = ", medium hair, split-color hair, brown hair, purple hair, straight hair, hair strand, \
-	hair wings, crossed bangs,  looking at viewer, fox_ears, green eyes, glowing eyes, boots, \
-	simple background, white background"
+strBeforeEmoji = "masterpiece, best quality, highres, line art, comic, anime, colorful,\
+	solo, upper body, kawaii, 1girl, small breasts, young,\
+	close front shoot symmetrical photo portrait,\
+	detailed eyes, glowing eyes, heterochromia green golden,\
+	emoji,chibi,"
+strAfterEmoji = "medium hair, red hair, straight hair, forehead,\
+	looking at viewer, fox_ears,\
+	simple background, white background,\
+	<lora:animeEMOJIStyleLora_v10:1>"
 
 def writeToFile(strFolderPath="./"):
 	#正向提示詞
 	strPromptsFilePath = os.path.join(strFolderPath, "prompts_file.txt")
 	with open(strPromptsFilePath, "w") as f:
 		for strEmoji in strEmojiList:
-			strPromptsLine = strBeforeEmoji + strEmoji + strAfterEmoji
+			strPromptsLine = strBeforeEmoji + "(" + strEmoji + ":1.8)," + strAfterEmoji
 			f.write(strPromptsLine + "\n")
 			print(strPromptsLine)
 
