dad0460032dd637b2ecbab4072a322afb30a180f
muchu1983 <muchu1983@gmail.com>
2023-03-22 13:55:15 +0800

    simpleify cyber paintbrush logic

--- src/core/cyber_paintbrush.py
+++ src/core/cyber_paintbrush.py
@@ -3,68 +3,34 @@
 """" """
 import os
 
-strEmojiList = ["(((angry)))",
-"(((cosy)))",
-"(((depressing)))",
-"(((disgusting)))",
-"(((embarrassing)))",
-"(((energetic)))",
-"(((evil)))",
-"(((fearful)))",
-"(((frightening)))",
-"(((grim)))",
-"(((guilty)))",
-"(((happy)))",
-"(((hopeful)))",
-"(((hopeless)))",
-"(((lonely)))",
-"(((lustful)))",
-"(((peaceful)))",
-"(((proud)))",
-"(((relieving)))",
-"(((romantic)))",
-"(((sad)))",
-"(((satisfying)))",
-"(((shameful)))",
-"(((surprising)))",
-"(((resist)))",
-"(((smile)))",
-"(((sigh)))",
-"(((cry)))",
-"(((cry hard)))",
-"(((move)))",
-"(((laughing out loud)))",
-"(((naughty)))",
-"(((wink)))",
-"(((scold)))",
-"(((glutton)))"]
+strEmojiList = ["((😀))","((🤬))","((😂))","((🙄))","((😴))","((😍))","((😜))","((😆))",\
+	"((😉))","((😵))","((😠))","((😎))","((🥱))","((🥶))","((🤮))","((😶))",\
+	"((😐))","((😨))","((🤡))","((😭))","((👿))","((💀))","((😇))","((😏))"]
 
-strBasicList = ["masterpiece", "best quality", "high resolution", "ultra detailed", "solo"]
-strCustomedList = ["1boy", "(cool boy face)", "in the forest", "dark hair", "short hair", "((symmetrical face))"]
-#strLoraSettingList = ["flat color", "big head style", "detailed eyes",
-#	"<lora:cartoonyStyle_cartoonyStyleV1:1>"]
+strNegative = "ng_deepnegative_v1_75t,  easynegative, (worst quality, low quality:1.4), logo, text, monochrome, NSFW, mutated hands and fingers, poorly drawn hands, ((bad anatomy))"
 
-strBeforeEmoji = "close front shoot symmetrical photo portrait of one young male werewolf"
-strAfterEmoji = " with wolf ears"
+strBeforeEmoji = "masterpiece, best quality, highres, line art, comic, anime, colorful, \
+	 solo, upper body, kawaii, 1girl, small breasts, Age:18, big head style, detailed eyes, \
+	 close front shoot symmetrical photo portrait"
+strAfterEmoji = ", medium hair, split-color hair, brown hair, purple hair, straight hair, hair strand, \
+	hair wings, crossed bangs,  looking at viewer, fox_ears, green eyes, glowing eyes, boots, \
+	simple background, white background"
 
 def writeToFile(strFolderPath="./"):
-	strFilePath = os.path.join(strFolderPath, "prompts_file.txt")
-	with open(strFilePath, "w") as f:
+	#正向提示詞
+	strPromptsFilePath = os.path.join(strFolderPath, "prompts_file.txt")
+	with open(strPromptsFilePath, "w") as f:
 		for strEmoji in strEmojiList:
+			strPromptsLine = strBeforeEmoji + strEmoji + strAfterEmoji
+			f.write(strPromptsLine + "\n")
+			print(strPromptsLine)
 
-			strPromptLine = strBeforeEmoji + strEmoji + strAfterEmoji + ", " + \
-				','.join(strBasicList) + ", " + \
-				','.join(strCustomedList)
-				#','.join(strCustomedList) + ", " + \
-				#','.join(strLoraSettingList)
-			f.write(strPromptLine + "\n")
-			print(strPromptLine)
-
+	#反向提示詞
+	strNegativeFilePath = os.path.join(strFolderPath, "negative_file.txt")
+	with open(strNegativeFilePath, "w") as f:
+		f.write(strNegative + "\n")
 
 
 if __name__ == '__main__':
 	writeToFile()
 
-""" negative
-ng_deepnegative_v1_75t,  easynegative, (worst quality, low quality:1.4), logo, text, monochrome, NSFW, mutated hands and fingers, poorly drawn hands, ((bad anatomy))
-"""
