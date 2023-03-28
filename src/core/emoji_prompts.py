e5044a12079e425e4a63e96de9d5e84de6035515
muchu1983 <muchu1983@gmail.com>
2023-03-28 20:41:52 +0800

    merge --negative and move emoji prompts to first and add prolist

--- src/core/emoji_prompts.py
+++ src/core/emoji_prompts.py
@@ -2,43 +2,44 @@
 # -*- coding: utf-8 -*-
 """" """
 import os
+strPromptsStart = '--prompt "'
+strProList = ['cra', 'eniripsa', 'iop', 'osamodas', 'sacrier', 'sram', 'xelor']
+strEmojiList = ['happy', 'sad', 'scared']
 
-#strEmojiList = ["((😀))","((🤬))","((😂))","((🙄))","((😴))","((😍))","((😜))","((😆))",\
-#   "((😉))","((😵))","((😠))","((😎))","((🥱))","((🥶))","((🤮))","((😶))",\
-#   "((😐))","((😨))","((🤡))","((😭))","((👿))","((💀))","((😇))","((😏))"]
-strEmojiList = ['blush', 'tongue out', 'tears', 'grin', 'embarrassed',\
-    'fangs', 'angry', 'smug', 'evil smile', 'sleepy' ]
+strPrompts = 'masterpiece, best quality, highres,\
+    1girl, beautiful, upper body, looking at viewer,\
+    extremely_detailed_eyes_and_face,\
+    ponytail, long hair, brown hair, forehead,\
+    high detailed skin,\
+    pants,\
+    simple background, white background,\
+    <lora:Toru8pWavenChibiStyle_wavenchibiLoraV10:0.8>, colored skin, bandages, " '
 
-strNegative = 'easynegative, worst quality, low quality, normal quality, lowres,\
-    blurry, girl, woman, female, exposed breast, nipples, NSFW,\
-    mutated hands and fingers, poorly drawn hands, bad hands,missing finger, extra digits, fewer digits,\
+strNegatives = ' --negative_prompt "easynegative, ng_deepnegative_v1_75t,\
+    worst quality, low quality, normal quality, lowres, blurry,\
+    exposed breast, nipples, NSFW, (bad anatomy:2),\
+    mutated hands and fingers, poorly drawn hands, bad hands, missing finger, extra digits, fewer digits,\
+    cockeye, walleye,\
     monochrome, logo, text, error, signature, watermark, username, artist name,\
-    tattoo, weapon, cockeye, walleye, (bad anatomy:2), '
+    tattoo, weapon,\
+    turn pale, gloom (expression), "'
 
-strBeforeEmoji = 'masterpiece, best quality, highres,\
-    <lora:Moxin_10:0.5>,  shuimobysim, badashanren,\
-    <lora:reinhardtOverwatch_v1:0.8>, 1man, old man, reinhardt,\
-    (muscular male:1.3), strong body, male focus, abdominal muscles, '
-strAfterEmoji = 'extremely_detailed_eyes_and_face, detailed eyes, Fire in eyes, extremely detailed eyes,\
-    ponytail, long hair, brown hair, forehead, \
-    beard,  goatee, stubble, \
-    upper body, high detailed skin,  looking at viewer,\
-    (topless male:1.2), shirtless ,pants,\
-    simple background, white background, '
+
+
 
 def writeToFile(strFolderPath="./"):
-    #正向提示詞
+    #提示詞
     strPromptsFilePath = os.path.join(strFolderPath, "line_prompts_file.txt")
     with open(strPromptsFilePath, "w") as f:
-        for strEmoji in strEmojiList:
-            strPromptsLine = strBeforeEmoji + "(" + strEmoji + ":1.5)," + strAfterEmoji
-            f.write(strPromptsLine + "\n")
-            print(strPromptsLine)
-
-    #反向提示詞
-    strNegativeFilePath = os.path.join(strFolderPath, "line_negative_file.txt")
-    with open(strNegativeFilePath, "w") as f:
-        f.write(strNegative + "\n")
+        for strPro in strProList:
+            for strEmoji in strEmojiList:
+                strPromptsLine = strPromptsStart +\
+                "(" + strPro + ")," +\
+                "(" + strEmoji + ")," +\
+                strPrompts +\
+                strNegatives
+                f.write(strPromptsLine + "\n")
+                print(strPromptsLine)
 
 def main():
     writeToFile()
