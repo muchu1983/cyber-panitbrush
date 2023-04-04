fee0200adc97a2f830a642e2765cb6be2e1fae66
muchu1983 <muchu1983@gmail.com>
2023-04-04 11:15:00 +0800

    add more face prompt

--- src/core/emoji_prompts.py
+++ src/core/emoji_prompts.py
@@ -6,7 +6,16 @@ strHairColorList = ['red hair', 'green hair', 'blue hair',]
 strEmojiList = ['happy', 'sad', 'scared', 'angry', 'cosy', 'depressing', 'disgusting', 'embarrasing', 'energetic',
     'evil', 'fearful', 'frightening', 'grim', 'guilty', 'hopeful', 'hopeless', 'lonely', 'lustful', 'peaceful',
     'proud', 'relieving', 'romantic', 'resist', 'smile', 'sigh', 'cry', 'cry hard', 'move', 'laughing out loud',
-    'naughty', 'wink', 'scold', 'glutton']
+    'naughty', 'wink', 'scold', 'glutton', 'blush', 'smile', 'open mouth', 'red eyes', 'closed eyes', ':d',
+    'parted lips', 'one eye closed', 'teeth', 'tongue', 'fang', 'tears', 'tongue out', 'grin', ':o', 'v-shaped eyebrows',
+    'symbol-shaped pupils', 'saliva', 'facial hair', 'embarrassed', 'fangs', 'mouth hold', 'heart-shaped pupils',
+    'wavy mouth', 'half-closed eyes', 'trembling', ';d', 'crying', '> <', 'slit pupils', 'clenched teeth', 'drooling',
+    'covering', 'glowing eyes', 'anger vein', ':p', 'tareme', 'heavy breathing', 'skin fang', 'tearing up', 'naughty face',
+    ':q', 'crying with eyes open', 'o o', '+ +', 'serious', 'smirk', '@ @', 'full-face blush', 'pout', 'raised eyebrows',
+    'covering mouth', 'licking lips', 'wince', 'ahegao', 'food on face', 'saliva trail', 'bags under eyes',
+    'star-shaped pupils', 'nosebleed', 'annoyed', ':/', 'sleepy', 'nervous', 'ear blush', 'covering face', 'moaning',
+    'solid circle eyes', 'glaring', 'crazy eyes', 'streaming tears', 'half-closed eye', 'heart in eye', 'mouth drool',
+    'screaming', 'smelling', 'happy tears', 'cheek bulge', 'spit take', 'upturned eyes', 'sad smile']
 strPromptsStart = '--prompt "'
 strNegativesStart = '--negative_prompt "'
 strPnNEnd = '" '
@@ -20,7 +29,7 @@ strPrompts = 'masterpiece, best quality, highres,\
 strNegatives = 'worst quality, low quality, normal quality, lowres, blurry,\
     badhandv4, bad anatomy, cockeye, walleye,\
     nsfw, monochrome, logo, text, error, signature, watermark, username, artist name,\
-    turn pale, gloom \\(expression\\),'
+    turn pale, gloomy, '
 
 def writeToFile(strFolderPath='./'):
     #提示詞
